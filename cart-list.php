3900d9d3238680618ed57c4cb4d2e27110f84808
Ahmadfathy <ahmadfathy77@gmail.com>
2019-06-03 16:12:50 +0200

    added cart list

---
+++ cart-list.php
@@ -0,0 +1,269 @@
+<?php $pageTitle = 'عربة التسوق'; ?>
+<?php include 'init.php'; ?>
+
+<!--==============Inner Banner Start==============-->
+<?php include $tpl . 'pages-banner.php';?>
+<!--==============Inner Banner End================-->
+
+
+<!--==============	Main Start ================-->
+<main class="inner__content" role="main">
+  <div class="container">
+    <div class="pt-80">
+      <div class="row">
+        <div class="col-12">
+          <h5 class="text-primary mb-3">عربة التسوق <span>(<strong>4</strong> منتجات)</span></h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-sm-8">
+          <div class="inner__card shadow-sm mb-3">
+            <div class="card-body">
+              <div class="product-box">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-product">
+                      <img src="<?php echo $img; ?>labtop.jpg" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <p class="product-info">
+                      موبايل اوبو P11 بشريحتي اتصال و 4 كاميرات، جيل رابع، 6جيجا رام.
+                    </p>
+                    <h5 class="product-price text-primary mb-4">13,700,00</h5>
+                    <div class="row">
+                      <div class="col-6">
+                        <div class="float-left">
+                          <div class="cart-counter">
+                            <button id="increase" onclick="increaseValue()" value="Increase Value" class="item-increase">
+                              <i class="fas fa-plus"></i>
+                            </button>
+                            <input type="text" id="number" value="0"  class="count text-center"/>
+                            <button id="decrease" onclick="decreaseValue()" value="Decrease Value" class="item-decrease">
+                              <i class="fas fa-minus"></i>
+                            </button>
+                          </div><!-- /.cart-counter -->
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="float-right">
+                          <a href="#" class="text-left remove">مسح</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="inner__card shadow-sm mb-3">
+            <div class="card-body">
+              <div class="product-box">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-product">
+                      <img src="<?php echo $img; ?>labtop.jpg" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <p class="product-info">
+                      موبايل اوبو P11 بشريحتي اتصال و 4 كاميرات، جيل رابع، 6جيجا رام.
+                    </p>
+                    <h5 class="product-price text-primary mb-4">13,700,00</h5>
+                    <div class="row">
+                      <div class="col-6">
+                        <div class="float-left">
+                          <div class="cart-counter">
+                            <button id="increase" onclick="increaseValue()" value="Increase Value" class="item-increase">
+                              <i class="fas fa-plus"></i>
+                            </button>
+                            <input type="text" id="number" value="0"  class="count text-center"/>
+                            <button id="decrease" onclick="decreaseValue()" value="Decrease Value" class="item-decrease">
+                              <i class="fas fa-minus"></i>
+                            </button>
+                          </div><!-- /.cart-counter -->
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="float-right">
+                          <a href="#" class="text-left remove">مسح</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="inner__card shadow-sm mb-3">
+            <div class="card-body">
+              <div class="product-box">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-product">
+                      <img src="<?php echo $img; ?>labtop.jpg" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <p class="product-info">
+                      موبايل اوبو P11 بشريحتي اتصال و 4 كاميرات، جيل رابع، 6جيجا رام.
+                    </p>
+                    <h5 class="product-price text-primary mb-4">13,700,00</h5>
+                    <div class="row">
+                      <div class="col-6">
+                        <div class="float-left">
+                          <div class="cart-counter">
+                            <button id="increase" onclick="increaseValue()" value="Increase Value" class="item-increase">
+                              <i class="fas fa-plus"></i>
+                            </button>
+                            <input type="text" id="number" value="0"  class="count text-center"/>
+                            <button id="decrease" onclick="decreaseValue()" value="Decrease Value" class="item-decrease">
+                              <i class="fas fa-minus"></i>
+                            </button>
+                          </div><!-- /.cart-counter -->
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="float-right">
+                          <a href="#" class="text-left remove">مسح</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="inner__card shadow-sm mb-3">
+            <div class="card-body">
+              <div class="product-box">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-product">
+                      <img src="<?php echo $img; ?>labtop.jpg" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <p class="product-info">
+                      موبايل اوبو P11 بشريحتي اتصال و 4 كاميرات، جيل رابع، 6جيجا رام.
+                    </p>
+                    <h5 class="product-price text-primary mb-4">13,700,00</h5>
+                    <div class="row">
+                      <div class="col-6">
+                        <div class="float-left">
+                          <div class="cart-counter">
+                            <button id="increase" onclick="increaseValue()" value="Increase Value" class="item-increase">
+                              <i class="fas fa-plus"></i>
+                            </button>
+                            <input type="text" id="number" value="0"  class="count text-center"/>
+                            <button id="decrease" onclick="decreaseValue()" value="Decrease Value" class="item-decrease">
+                              <i class="fas fa-minus"></i>
+                            </button>
+                          </div><!-- /.cart-counter -->
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="float-right">
+                          <a href="#" class="text-left remove">مسح</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="inner__card shadow-sm mb-3">
+            <div class="card-body">
+              <div class="product-box">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-product">
+                      <img src="<?php echo $img; ?>labtop.jpg" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <p class="product-info">
+                      موبايل اوبو P11 بشريحتي اتصال و 4 كاميرات، جيل رابع، 6جيجا رام.
+                    </p>
+                    <h5 class="product-price text-primary mb-4">13,700,00</h5>
+                    <div class="row">
+                      <div class="col-6">
+                        <div class="float-left">
+                          <div class="cart-counter">
+                            <button id="increase" onclick="increaseValue()" value="Increase Value" class="item-increase">
+                              <i class="fas fa-plus"></i>
+                            </button>
+                            <input type="text" id="number" value="0"  class="count text-center"/>
+                            <button id="decrease" onclick="decreaseValue()" value="Decrease Value" class="item-decrease">
+                              <i class="fas fa-minus"></i>
+                            </button>
+                          </div><!-- /.cart-counter -->
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="float-right">
+                          <a href="#" class="text-left remove">مسح</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-4">
+        <div class="total-box">
+          <h3 class="text-primary">13,700,00</h3>
+          <h6>السعر الكلي</h6>
+          <form action="" method="post">
+            <div class="form-row mb-3">
+              <div class="col-5 col-sm-3">
+                <label class="mt-2" for="">كود الخصم</label>
+              </div>
+              <div class="col-7 col-sm-9">
+                <input type="text" name="" id="" class="form-control">
+              </div>
+            </div>
+            <div class="form-row mb-3">
+              <div class="col-12">
+                <button class="btn btn-primary btn-block btn-lg" type="submit">تكملة عملية الدفع</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<!--=============== Main End ==================-->
+
+
+<?php include $tpl . 'footer.php'; ?>
+
+	<script>
+	var balance = 107;
+        function increaseValue() {
+            var value = parseInt(document.getElementById('number').value, 10);
+            value = isNaN(value) ? 0 : value;
+            value++;
+	    if(value>balance){
+		alert("this item only have "+balance+" items in the stock.");
+		value--;
+		return ;
+		}
+            document.getElementById('number').value = value;
+        }
+
+        function decreaseValue() {
+            var value = parseInt(document.getElementById('number').value, 10);
+            value = isNaN(value) ? 0 : value;
+            value < 1 ? value = 1 : '';
+            value--;
+            document.getElementById('number').value = value;
+        }
+	</script>
+
+<?php include $tpl . 'end.php'; ?>
